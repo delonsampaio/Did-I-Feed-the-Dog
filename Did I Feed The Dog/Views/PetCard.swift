c71ed7f0a0166dc4c2e6851e780b12ac9a070e44
Delon Sampaio <delon.sampaio@starlims.com>
2026-04-30 11:12:59 -0400

    fix: use WidgetDataWriter on undo so widget data file is updated, not just reloaded

--- Did I Feed The Dog/Views/PetCard.swift
+++ Did I Feed The Dog/Views/PetCard.swift
@@ -91,7 +91,7 @@ struct PetCard: View {
                             }
                         }
                         modelContext.delete(event)
-                        WidgetCenter.shared.reloadAllTimelines()
+                        WidgetDataWriter.write(from: modelContext)
                     }
                     showUndoToast = false
                     lastLoggedEvent = nil
